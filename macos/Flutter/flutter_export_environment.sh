48bae34d520fc507f1da9666d7673e5335d54c85
ym7996 <ym7996@closeli.cn>
2019-08-07 18:45:05 +0800

    <feat>  : 引入macOS版本

---
+++ macos/Flutter/flutter_export_environment.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/lifei/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/lifei/flutter-desktop-embedding/example"
+export "FLUTTER_TARGET=/Users/lifei/flutter-desktop-embedding/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=/Users/lifei/flutter-desktop-embedding/example/.dart_tool/flutter_build/1bf5b5241fc44ee608dc7873a422a0d9"
+export "FLUTTER_FRAMEWORK_DIR=/Users/lifei/flutter/bin/cache/artifacts/engine/darwin-x64"

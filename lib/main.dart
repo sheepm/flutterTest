e52a49ae840888979350618cfadd176d9dafe407
yangmo <redswing@foxmail.com>
2019-08-20 18:46:24 +0800

    <feat> : layout

--- lib/main.dart
+++ lib/main.dart
@@ -5,129 +5,53 @@ import 'package:flutter/material.dart';
 void main() {
   // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
-
-  runApp(new FadeAppTest());
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SampleAppPage(),
-    );
-  }
-}
+      title: 'hello',
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: AspectRatio(aspectRatio: 20),
+              color: Colors.red,
+            ),
+            Expanded(
+              flex: 10,
+              child: Container(
+                color: Colors.black,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.deepOrange,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 4,
+                      child: Container(
+                        color: Colors.greenAccent,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              child: AspectRatio(aspectRatio: 20, child: Row(
 
-class SampleAppPage extends StatefulWidget{
-
-  @override
-  State<StatefulWidget> createState() {
-    return _SampleAppPageState();
-  }
-
-}
-
-class _SampleAppPageState extends State<SampleAppPage>{
-
-  bool toggle = true;
-  void _toggle(){
-    setState(() {
-      toggle = !toggle;
-    });
-  }
-
-  _getToggleChild(){
-    if(toggle){
-      return Text('toggle one');
-    }else{
-      return MaterialButton(onPressed: (){}, child: Text('toggle two'));
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Sample App"),
-      ),
-      body: Center(
-        child: _getToggleChild(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _toggle,
-        tooltip: 'update text',
-        child: Icon(Icons.update),
-      ),
-    );
-  }
-
-}
-
-class FadeAppTest extends StatelessWidget{
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fade Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: MyFadeTest(title : 'Fade demo'),
-    );
-  }
-
-}
-
-class MyFadeTest extends StatefulWidget{
-  MyFadeTest({Key key, this.title}) : super(key : key);
-
-  final String title;
-
-  @override
-  State<StatefulWidget> createState() {
-    return _MyFadeTest();
-  }
-
-}
-
-class _MyFadeTest extends State<MyFadeTest> with TickerProviderStateMixin{
-  AnimationController controller;
-  CurvedAnimation curve;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 2000));
-    curve = CurvedAnimation(parent: controller, curve: Curves.easeIn);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Container(
-          child: FadeTransition(
-              opacity: curve,
-              child: FlutterLogo(
-                size: 100.0,
-          ),
-          ),
+              ),),
+              color: Colors.yellowAccent,
+            )
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Fade',
-          child: Icon(Icons.brush),
-          onPressed: (){
-            controller.forward();
-          }),
     );
   }
-
 }
